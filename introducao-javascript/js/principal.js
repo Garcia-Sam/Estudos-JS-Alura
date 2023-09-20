bc7e39b1dfed7e1397ff7f8e8e6ae068114b094e
Samuel Gracia <macedosam55@gmail.com>
2023-09-20 10:08:28 -0300

    Update principal.js

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -2,41 +2,47 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = ("Nutri Aparecida");
 
 // Selecionador de paciente
-var paciente = document.querySelector("#primeiro-paciente")
+var pacientes = document.querySelectorAll(".paciente")
 
-// Selecionador de Peso
-var tdPeso = paciente.querySelector(".info-peso")
-var pesoValido = true
-var peso = tdPeso.textContent
 
-// Selecionador de Altura
-var tdAltura = paciente.querySelector(".info-altura")
-var alturaValida = true
-var altura = tdAltura.textContent
+for(var i = 0; i < pacientes.length; i++){
+    
+    var paciente = pacientes[i]
 
-// Alteração no valor do IMC direto na tabela
-var tdImc = paciente.querySelector(".info-imc")
-
-if (peso <= 0 || peso >= 1000){
-    console.log("Peso Inválido!")
-    pesoValido = false
-    tdImc.textContent = "Peso Inválido!"
-}
-
-if (altura <= 0 || altura >= 4.00){
-    console.log("Altura Inválida!")
-    alturaValida = false
-    tdImc.textContent = "Altura Inválida"
-}
-
-if (pesoValido == false && alturaValida == false){
-    console.log("Peso e Altura Inválidos!")
-    tdImc.textContent = "Peso e Altura Inválidos!"
-}
-
-// Calculo do IMC
-if (pesoValido && alturaValida){
-    var imc = peso / (altura * altura)
-    tdImc.textContent = imc
+    // Selecionador de Peso
+    var tdPeso = paciente.querySelector(".info-peso")
+    var pesoValido = true
+    var peso = tdPeso.textContent
+    
+    // Selecionador de Altura
+    var tdAltura = paciente.querySelector(".info-altura")
+    var alturaValida = true
+    var altura = tdAltura.textContent
+    
+    // Alteração no valor do IMC direto na tabela
+    var tdImc = paciente.querySelector(".info-imc")
+    
+    if (peso <= 0 || peso >= 1000){
+        console.log("Peso Inválido!")
+        pesoValido = false
+        tdImc.textContent = "Peso Inválido!"
+    }
+    
+    if (altura <= 0 || altura >= 4.00){
+        console.log("Altura Inválida!")
+        alturaValida = false
+        tdImc.textContent = "Altura Inválida"
+    }
+    
+    if (pesoValido == false && alturaValida == false){
+        console.log("Peso e Altura Inválidos!")
+        tdImc.textContent = "Peso e Altura Inválidos!"
+    }
+    
+    // Calculo do IMC
+    if (pesoValido && alturaValida){
+        var imc = peso / (altura * altura)
+        tdImc.textContent = imc.toFixed(2)
+    }
 }
 
