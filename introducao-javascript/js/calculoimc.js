5dfb05c01cda5943110c9d41ee86e6db4c605217
Samuel Gracia <89318578+Garcia-Sam@users.noreply.github.com>
2023-09-25 16:21:59 -0300

    Update calculo.imc

--- introducao-javascript/js/calculoimc.js
+++ introducao-javascript/js/calculoimc.js
@@ -10,39 +10,40 @@ for(var i = 0; i < pacientes.length; i++){
 
     // Selecionador de Peso
     var tdPeso = paciente.querySelector(".info-peso")
-    var pesoValido = true
     var peso = tdPeso.textContent
     
     // Selecionador de Altura
     var tdAltura = paciente.querySelector(".info-altura")
-    var alturaValida = true
     var altura = tdAltura.textContent
     
     // Alteração no valor do IMC direto na tabela
     var tdImc = paciente.querySelector(".info-imc")
+
+    var pesoEhValido = validaPeso(peso)
+    var alturaEhValida = true
     
-    if (peso <= 0 || peso >= 1000){
+    if (!pesoEhValido){
         console.log("Peso Inválido!")
-        pesoValido = false
+        pesoEhValido = false
         tdImc.textContent = "Peso Inválido!"
         paciente.classList.add("paciente-invalido")
     }
     
     if (altura <= 0 || altura >= 4.00){
         console.log("Altura Inválida!")
-        alturaValida = false
+        alturaEhValida = false
         tdImc.textContent = "Altura Inválida"
         paciente.classList.add("paciente-invalido")
     }
     
-    if (pesoValido == false && alturaValida == false){
+    if (pesoEhValido == false && alturaEhValida == false){
         console.log("Peso e Altura Inválidos!")
         tdImc.textContent = "Peso e Altura Inválidos!"
         paciente.classList.add("paciente-invalido")
     }
     
     // Calculo do IMC
-    if (pesoValido && alturaValida){
+    if (pesoEhValido && alturaEhValida){
         var imc = calculaImc(peso, altura)
         tdImc.textContent = imc
     }
@@ -60,4 +61,12 @@ function calculaImc(peso, altura){
     var imc = 0
     imc = peso / (altura * altura)
     return imc.toFixed(2)
+}
+
+function validaPeso(peso) {
+    if (peso >= 0 && peso < 1000) {
+        return true
+    } else {
+        return false
+    }
 }
